9556fe701c78b2bc67e6c38e0686d9b35b5cf181
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-05-31 16:12:27 +0200

    webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import './styles.css'
+import IMAGE from './bank.png'
+import CALENDAR from './calendar.svg'
+
 export const App = () => {
-    return <h1>React Typescript Starter Template</h1>
+    return <>
+    <img src={IMAGE} alt="" width="300" />
+    <img src={CALENDAR} alt="" width="300" />
+
+    <h1>React Typescript Starter Template - 
+        {process.env.NODE_ENV} {process.env.NAME}
+        </h1>
+    </>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
-import ReactDOM from 'react-dom';
+// /import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+
+const root = createRoot(container!)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />)
+// ReactDOM.render(<App />, document.getElementById('root'));
