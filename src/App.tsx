7a4cb7520e5f5fd5623834ec95ca8699dc67ab7c
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-06-01 13:30:50 +0200

    react-refresh, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './styles.css'
 import IMAGE from './bank.png'
 import CALENDAR from './calendar.svg'
+import ClickCounter from './ClickCounter';
 
 export const App = () => {
-    return <>
+    
+    return (<>
     <img src={IMAGE} alt="" width="300" />
     <img src={CALENDAR} alt="" width="300" />
 
-    <h1>React Typescript Starter Template - 
-        {process.env.NODE_ENV} {process.env.NAME}
-        </h1>
+    {/* <h1>React Typescript Starter Template - 
+        {process.env.NODE_ENV}  
+        {process.env.name}
+        </h1> */}
+        hello
+        <ClickCounter />
     </>
+)
 }
